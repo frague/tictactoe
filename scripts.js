493b4a5d582f01cc40195ce271f214b415a5a15d
Nick Bogdanov <nikolay.bogdanov@gmail.com>
2014-08-04 17:24:26 +0400

    Adds initial files set w/o the server

--- scripts.js
+++ scripts.js
@@ -1,16 +1,18 @@
-var isX = true, finished = false, cells = {}, moves = [], checkLines = [[0, 1], [1, 0], [1, 1], [1, -1]], moves = document.getElementById('moves');
+var name, sessionId, db, ref, isX = true, finished = false, cells = {}, moves = [], checkLines = [[0, 1], [1, 0], [1, 1], [1, -1]], movesList = document.getElementById('moves');
 
-function move(td) {
-    var x = +td.id[1], y = +td.id[2], key = '' + x + y, sign = isX ? 'x': 'o';
+function move(td, sign) {
+    var x = +td.id[1], y = +td.id[2], key = '' + x + y, sign = sign ? sign : isX ? 'x': 'o';
     if (!cells[key] && !finished) {
+        moves.push([td.id, sign]);
         cells[key] = sign;
         td.className = sign;
-        isX = !isX;
+
+        ref.child(moves.length).setWithPriority({move: td.id, sign: sign, name: name}, moves.length);
+        isX = sign == 'o';
         // Check wins
         for (var i = 0; i < 4; i++) {
             var count = 0, d = checkLines[i], x1 = x - 2 * d[0], y1 = y - 2 * d[1], tds = [];
             for (j = 0; j < 5; j++) {
-                console.log(i, j);
                 var key = '' + x1 + y1, c = cells[key];
                 if (c) {
                     if (c == sign && count < 3) {
@@ -29,9 +31,65 @@ function move(td) {
             }
         }
         // Update UI
-        document.getElementById('move').innerText = isX ? 'x': 'o';
+        document.getElementById('move').innerText = name + ' ' + isX ? 'x': 'o';
         var m = document.createElement('li');
         m.innerHTML = sign + ' &rarr; ' + (x + 1) + ' : ' + (y + 1);
-        moves.appendChild(m);
+        movesList.appendChild(m);
     }
+}
+
+function setVisibility(id, state) {
+    var el = document.getElementById(id);
+    if (el) {
+        el.className = state ? '': 'hidden';
+    }
+}
+
+function createCookie(key, value, days) {
+    document.cookie = key + '=' + value;
+    console.log(document.cookie);
+}
+
+function readCookie(key) {
+    var value = '';
+    var cookieArray = document.cookie.split(';');
+    console.log(document.cookie);
+    for (var i = 0; i < cookieArray.length; i++) {
+        var keyValuePair = cookieArray[i].split('=');
+        if (keyValuePair[0] == key) {
+            value = keyValuePair[1];
+            break;
+        }
+    }
+    return value;
+}
+
+function start(id) {
+    db = new Firebase('https://burning-fire-3834.firebaseio.com');
+    name = document.getElementById("name").value || name;
+    createCookie('xoname', name);
+    console.log(name);
+
+    if (id) {
+        sessionId = id;
+    } else {
+        sessionId = '';
+        for (var i = 0; i < 20; i++) sessionId += String.fromCharCode(i % 2 ? 65 + 26 * Math.random(): 49 + 9 * Math.random());
+        window.location.hash = sessionId;
+    }
+    setVisibility('welcome', false);
+    setVisibility('playground', true);
+
+    ref = db.child(sessionId);
+    ref.on('child_added', function(i) {
+        move(document.getElementById(i.val().move), i.val().sign);
+    })
+}
+
+window.onload = function() {
+    name = readCookie('xoname');
+    document.getElementById('name').value = name;
+    console.log(name);
+
+    if (window.location.hash) start(window.location.hash.replace('#', ''));
 }
